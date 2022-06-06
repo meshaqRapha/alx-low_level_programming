76a131d5d1f1b9a8978b11286d5ff59b8511bc7d
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-06 15:53:55 -0700

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0
+  */
+
+int main(void)
+{
+
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+if (x == 'q')
+{
+	continue;
+}
+
+if (x == 'e')
+{
+	continue;
+}
+
+putchar(x);
+putchar('\n');
+}
+
+
+return (0);
+}
