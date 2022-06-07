f92a535c75baede07c52319ed89fe4f387f06f7c
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-06 17:28:44 -0700

    all single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: 0
+  */
+
+int main(void)
+{
+int x;
+
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+return (0);
+}
