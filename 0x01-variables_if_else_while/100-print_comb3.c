37061dd7265182b4a399ab7eba50467e20483047
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-06 17:04:50 -0700

    Not two combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: 0
+  */
+
+int main(void)
+{
+int x;
+int y;
+
+for (x = '0'; x <= '9'; x++)
+{
+for (y = x + 1; y <= '9'; y++)
+{
+putchar(x);
+putchar(y);
+
+if (x + '8')
+{
+	putchar(',');
+	putchar(' ');
+
+}
+}
+}
+putchar('\n');
+return (0);
+}
