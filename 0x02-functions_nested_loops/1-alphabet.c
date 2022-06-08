b19c9a660317cb2037a96702c88ab28fbed1c2fc
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-08 08:56:24 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
-  *
+  * print_alphabet - prints all the alphabets in lower case
   * Return: void
   */
 void print_alphabet(void)
-{	
+{
 char alpha = 'a';
-	
+
 while (alpha <= 'z')
 {
 _putchar(alpha);
 alpha++;
 }
-
 _putchar('\n');
 }
