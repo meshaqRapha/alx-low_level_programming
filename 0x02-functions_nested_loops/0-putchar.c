00aee3d9f62d5211a78d04a76641bb523b930bde
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-08 15:18:33 -0700

    Pootkar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
+  * main - Entry point
   * _putchar - writes a character c to stdout
-  * @c: chatacter to be printef out
+  * @c: chatacter to be printed
   * Return: void
   */
 
+int main(void)
+{
 int _putchar(char c)
 {
 char str[] = "_putchar";
@@ -17,4 +21,7 @@ while (str[c] != '\0')
 _putchar(str[c]);
 c++;
 }
+}
+
+return (0);
 }
