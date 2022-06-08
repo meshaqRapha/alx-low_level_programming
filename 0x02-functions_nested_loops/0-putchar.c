ef90b6d545c39ad0d7a2eeaa2b62955467e44ad3
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-08 08:09:48 -0700

    Pootkar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *
+  *
+  * Return: void
+  */
+
+int _putchar(char c)
+
+char str[] = "_putchar";
+
+i = 0;
+
+while (str[i]) != '\0'
+{
+_putchar(str[i];
+i++;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+int _putcha(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
