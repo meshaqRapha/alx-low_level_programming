e8dc82fc72dee4345d41caed7df6ab1f6f0a8339
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-08 09:46:39 -0700

    times ten

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - prints the alphabets in lower case ten times followed by a new line
+  *
+  * Return: void
+  */
+
+void print_alphabet_x10(void);
+{
+int n = 0;
+
+while (n <= 10)
+{
+int m = 'a';
+
+while (m <= 'z')
+{
+putchar(m);
+m++;
+}
+
+putchar("\n");
+n++;
+}
+
+return (void);
+}
