95cffc4a2eba63821109b7806c32bf498dbbce07
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-10 06:33:57 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _isdigit - checks for a digit
+  * @c: character to be checked
+  * Return: 1 if digit && 0 if otherwise
+  */
+
+int _isupper(int c)
+{
+if (c = '0' && c >= '9')
+{
+	return (1);
+}
+
+else
+{
+	return (0);
+}
+_putchar('\n');
+}
