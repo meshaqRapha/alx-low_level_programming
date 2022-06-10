f73b3bba76bb6fee1a44065c77244c04cb602c9b
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-10 06:03:49 -0700

    Upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,16 +8,17 @@
 
 int _isupper(int c)
 {
-int c;
+char c;
 
-if (c >= 'A' && c <= 'Z')
+if (c >= 41 && c <= 90)
 {
 	return (1);
 }
 
-else
+else if (c >= 97 && c <= 122)
 {
 	return (0);
 }
 
+_putchar('\n');
 }
