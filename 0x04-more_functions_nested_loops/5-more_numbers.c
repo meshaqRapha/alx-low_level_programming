71e92b7e91dd5d7f25b806bc8f4d2c0b306d862d
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-10 15:57:40 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints 0 to 14 ten times
+  *
+  * Return: 0
+  */
+
+void more_numbers(void)
+{
+	int nums, line;
+
+	for (lines = 0; lines <= 9; lines++)
+	{
+	for (nums = 0; nums <= 14; nums++)
+	{
+		if (nums > 9)
+		{
+		_putchar((num / 10) + '0');
+		}
+	_putchar((num % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
