f0eaf7fe24a7544183077e9d2b1e950fdcf9a91c
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-10 06:52:25 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+/**
+  * mul - multiplies two integers
+  * @a: first integer
+  * @b: second integer
+  * Return: Always 0
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+	_putchar('\n');
+}
