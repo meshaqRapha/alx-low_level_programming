d729226b2f8f7d91d9070e739bec4ac39fd47551
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-13 21:33:10 -0700

    First commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
   * reset_to_98 - resets a pointer to 98
-  * @*n: the pointer to reset
+  * @n: the pointer to reset
   * Return: Always 0
   */
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define main_h
 
 int _putchar(char c);
-void reset_to_98(int *n):
-void swap_int(int *a, int *b):
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
