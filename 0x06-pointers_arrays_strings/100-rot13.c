fa5a8439f39e49a6d03517ab42cd8d4422cbba0c
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-16 16:29:09 -0700

    fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,7 @@
 char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
-	
+
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 			     'G', 'H', 'I', 'J', 'K', 'L',
 			     'M', 'N', 'O', 'P', 'Q', 'R',
