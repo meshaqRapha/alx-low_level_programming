09cea17a96a57903a77e6cfacf18e1730583ac99
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-16 12:09:18 -0700

    Contenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates two strings
+ * _strcat - Concatenates two strings
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
