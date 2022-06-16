1c7a3bbc79b3de6b125b5609984307b1c9b99fb2
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-16 15:57:30 -0700

    encode rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * rot13 - Encodes a string using rot13.
@@ -10,6 +10,7 @@ include "main.h"
 char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
+	
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 			     'G', 'H', 'I', 'J', 'K', 'L',
 			     'M', 'N', 'O', 'P', 'Q', 'R',
