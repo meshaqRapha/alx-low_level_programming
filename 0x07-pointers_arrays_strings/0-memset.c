73b1c5f966c34b57d98a3bacf93246462f933dfc
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-06-21 10:12:18 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fillsthe first n bytes of the memory area with constant byte
+ * @n: number of bytes
+ * @s: the memory area pointed to
+ * @b: constant byte
+ * Return: s
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	char *s;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	
+	return (s[i]);
+}
