c8870bcb0db100772920bb960de17a12af0b2b8c
nwakpa mishak <meshaqrapha@gmail.com>
2022-06-30 07:24:11 -0700

    Task5.Helloprintf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
-int main(void)
+/*This is task 5,Hello printf*/
+int main(void)/*This is task 5,Hello printf*/
 {
-	/*This is task 5,Hello printf*/
 printf("with proper grammar, but the outcome is a piece of art,");
-return (o);
+return (0);
 }
