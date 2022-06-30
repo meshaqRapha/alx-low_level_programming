1e2925997b7d1a21e202c53d80cd1a515a5d4383
nwakpa mishak <meshaqrapha@gmail.com>
2022-06-30 12:46:56 -0700

    Task 4.4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main(void) /*Task4.Hello,puts*/
+/
+ *main - Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
