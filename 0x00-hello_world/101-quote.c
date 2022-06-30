568f28afc555b2e4d63f4c192d8f7da1ce98aba5
nwakpa mishak <meshaqrapha@gmail.com>
2022-06-30 14:21:31 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Description:and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+puts ("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+}
