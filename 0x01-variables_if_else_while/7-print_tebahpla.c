91b3fb450ec4efb4a70aafd6d8e2010198f0e3bf
nwakpa mishak <meshaqrapha@gmail.com>
2022-07-01 16:21:45 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in reverse
+ * followed by  new line
+ * Return: 0 if (successful)
+*/
+
+int main(void)
+{
+ char ch;
+
+ for (ch = 'z'; ch >= 'a'; ch--)
+ {
+  putchar(ch);
+ }
+ putchar('\n');
+ return (0);
+}
+
