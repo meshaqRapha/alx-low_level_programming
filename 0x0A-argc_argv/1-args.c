e180e3924c0eb707a7ed04715c5e76dc266fc290
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-07-04 13:06:00 -0700

    argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
   * Return: 0
   */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
