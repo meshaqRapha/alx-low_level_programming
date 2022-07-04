611037e8bf11261454a645eaca89d9e36d6a9597
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-07-04 12:39:02 -0700

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - program to print its name followed by a new line
+  * @argc: argument count
+  * @argv: argument vectors
+  * Return: 0;
+  */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
