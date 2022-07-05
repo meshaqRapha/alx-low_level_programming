52b20e9d3f08c39c88a65a305e9192d0f52df470
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-07-05 15:11:41 -0700

    Strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,20 +14,19 @@ unsigned int _strspn(char *s, char *accept)
 
 	while (*s)
 	{
-		for (index = 0; accept[index]; index++)
+	for (index = 0; accept[index]; index++)
+	{
+		if (*s == accept[index])
 		{
-			if (*s == accept[index])
-				{
-					bytes++;
-					break;
-				}
-
-				else if (accept[index + 1] == '\0')
-					return (bytes);
-			}
-
-			s++;
+			bytes++;
+			break;
 		}
 
-		return (bytes);
+		else if (accept[index + 1] == '\0')
+			return (bytes);
+	}
+	s++;
+	}
+
+	return (bytes);
 }
