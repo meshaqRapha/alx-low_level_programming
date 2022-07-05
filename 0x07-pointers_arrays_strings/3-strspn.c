381e7799d5799dc46647d109aac616263f1b68e5
ebu-be <ebubechi.anyanwu@yahoo.com>
2022-07-05 15:01:01 -0700

    Pointers and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: initial segment
+ * @accept: kdksl
+ * Return: s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	
+}
