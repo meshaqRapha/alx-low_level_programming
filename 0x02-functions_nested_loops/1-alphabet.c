6a5c482f9739762068af268baec02b2355b28f91
nwakpa mishak <meshaqrapha@gmail.com>
2022-07-05 16:32:22 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet -> prints the lowercase alphabets
+*
+*/
+
+void print_alphabet(void);
+{
+int abc;
+for (abc = 'a'; abc <= 'z' ; abc++)
+{
+_putchar(abc);
+}
+_putchar('\n);
+
+}
