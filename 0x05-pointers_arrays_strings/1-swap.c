352197f2c9e0da3fb3db9bbe28217b30ab645083
nwakpa mishak <meshaqrapha@gmail.com>
2022-07-12 14:55:41 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,14 +12,14 @@ void swap_int(int *a, int *b);
 
 int main(void)
 {
-int a;
-int b;
 
-a = 98;
-b = 42;
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
+	int temp;
+
+	temp = (*a);
+        (*a) = (*b);
+	(*b) = temp;
+	
 return (0);
+
 
 }
