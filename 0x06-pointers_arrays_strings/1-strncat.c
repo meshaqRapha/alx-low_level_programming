181eeff9aab60d4954748551fa2ab25e7e0fedb3
nwakpa mishak <meshaqrapha@gmail.com>
2022-07-14 18:41:03 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,8 @@
 
 /**
 * _strncat - concatenates n bytes from a string to another
-* @dest: destination string * @src: source string
+* @dest: destination string
+* @src: source string
 * @n: number of bytes of str to concatenate
 *
 * Return: a pointer to the resulting string dest
